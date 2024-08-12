f6dcfc075a806b09a2fb0652149f3d50762028a9
p-shrivastava <prabal.shrivastava@priceline.com>
2024-08-12 15:22:26 +0530

    Added Examples

---
+++ src/main/java/interview/bny/Sol.java
@@ -0,0 +1,20 @@
+package interview.bny;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Sol {
+
+  public static void main(String[] args) {
+    List<String> names = Arrays.asList("John", "John", "Tushar", "RAM", "Ram", "Sagar");
+    Map<String,Integer> map = new HashMap<>();
+//    names.stream().map(String::toLowerCase). do it in parallel streams
+//    order payment seller notification are 4 microservices and notification can only be called once the order payment seller are returning a response(called in parallel and how to handle if any 1 microservice is failing
+//    result should be :- {john = 2, tushar = 1, ram = 2, sagar = 1}]
+//    how to do DB partitioning based on location
+//    Indexing,views
+//    template pattern.
+  }
+}
---
+++ src/main/java/interview/pricevra/Sol.java
@@ -0,0 +1,122 @@
+package interview.pricevra;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
+import streams.IntStreams;
+
+public class Sol {
+
+  //  +++++
+//
+//Arpit 8000 50
+//Pushpak 4000 35
+//Arpit 6000 30
+//Arpit 5000 40
+//Arpit 5000 30
+//
+//O/P
+//
+//Arpit 5000 30
+//Arpit 5000 40
+//Arpit 6000 30
+//Arpit 8000 50
+//Pushpak 4000 35
+  static Queue<Integer> queue = new LinkedList<>();
+
+  public static void main(String[] args) {
+//    List<Employee> employeeList = new ArrayList<>();
+//    employeeList.sort((employee, t1) -> {
+//      if (employee.name.compareTo(t1.name) == 0) {
+//        if (employee.salary.compareTo(t1.salary) == 0) {
+//          return employee.age.compareTo(t1.age);
+//        } else {
+//          return employee.salary.compareTo(t1.salary);
+//        }
+//      } else {
+//        return employee.name.compareTo(t1.name);
+//      }
+//    });
+
+    //Teacher and a student -> A teacher can take up 5 assignments at a time but it can review only 1 assignment.
+    //List of student submitting the assignment.Students are coming in parallel
+
+//    while (true){
+//      if (queue.size() <= 5){
+//
+//      }
+//    }
+
+    //thers is 1 thread ie consuming -> Polling the data out of the Queue
+    //thers is n thread ie producing -> Putting the data in the Queue
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    list.add(7);
+    list.add(8);
+
+    list.parallelStream().forEach(Sol::add);
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(() -> {
+      Thread.sleep(1000);
+      return queue.remove();
+    });
+  }
+
+  //class A{ map<K,integer> }
+  public synchronized static void add(Integer integer) {
+    if (queue.size() <= 5) {
+      queue.add(integer);
+    } else {
+      try {
+        Thread.sleep(20000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  class Employee {
+
+    String name;
+    Integer salary;
+    Integer age;
+  }
+}
+
+
+class A<K> {
+
+  Map<K, Integer> map = new HashMap<K, Integer>();
+
+  public void add(K k) {
+    map.put(k, map.getOrDefault(k, 0) + 1);
+  }
+
+  public Map<K, Integer> sort() {
+    return map.entrySet().stream()
+        .sorted((kIntegerEntry, t1) -> kIntegerEntry.getValue().compareTo(t1.getValue()))
+        .collect(
+            Collectors.toMap(
+                kIntegerEntry -> kIntegerEntry.getKey(),
+                kIntegerEntry -> kIntegerEntry.getValue()
+            )
+        );
+  }
+}
+
+//A <- B C
+//J @Autowiring A
